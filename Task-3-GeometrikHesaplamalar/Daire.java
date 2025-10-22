e899bdb9feace581cf1318b10608d67200cdbfec
Ebubekir23 <250541051@firat.edu.tr>
2025-10-22 10:49:23 +0300

    Add files via upload

---
+++ Task-3-GeometrikHesaplamalar/Daire.java
@@ -0,0 +1,13 @@
+
+public class Daire {
+	public static void main(String [] args) {
+		double r = 5.5;
+		double pi = 3.14159;
+		double alan = pi*r*r;
+		double çevre = 2*pi*r;
+		System.out.println("Yarıçap: " + r);
+		System.out.println("Alan: " + alan);
+		System.out.println("Çevre: " + çevre);
+	}
+
+}
---
+++ Task-3-GeometrikHesaplamalar/Dikdörtgen.java
@@ -0,0 +1,12 @@
+
+public class Dikdörtgen {
+	public static void main(String [] args) {
+		double en = 4.5;
+		double boy = 7.9;
+		double alan = en*boy;
+		double cevre = en*2 + boy*2;
+		System.out.println("Alan: " + alan);
+		System.out.println("Çevre: " + cevre);
+	}
+
+}
