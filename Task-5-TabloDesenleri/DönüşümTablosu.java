cd48a67c9d20739ddc653b43c8fe5eedfa99d232
Ebubekir23 <250541051@firat.edu.tr>
2025-10-22 10:58:11 +0300

    Add files via upload

---
+++ Task-5-TabloDesenleri/DönüşümTablosu.java
@@ -0,0 +1,13 @@
+
+public class DönüşümTablosu {
+	public static void main(String [] args) {
+		System.out.println("Mil\tKilometre");
+        System.out.println("");
+        System.out.println("1\t" + (1*1.609));
+        System.out.println("5\t" + (5*1.609));
+        System.out.println("10\t" + (10*1.609));
+        System.out.println("20\t" + (20*1.609));
+        System.out.println("50\t" + (50*1.609));
+	}
+
+}
