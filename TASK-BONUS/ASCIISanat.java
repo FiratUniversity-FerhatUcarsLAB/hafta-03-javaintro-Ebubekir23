c16de4570ad5d7a95b938295d3e9b33c5e0f72fb
Ebubekir23 <250541051@firat.edu.tr>
2025-10-22 11:04:35 +0300

    Add files via upload

---
+++ TASK-BONUS/ASCIISanat.java
@@ -0,0 +1,12 @@
+
+public class ASCIISanat {
+	public static void main(String [] args) {
+		System.out.println("EEEEEEE   BBBBBB    U     U   BBBBBB    EEEEEEE   K     K    İ     RRRRRR");
+        System.out.println("E         B     B   U     U   B     B   E          K   K     İ     R     R");
+        System.out.println("EEE       BBBBBB    U     U   BBBBBB    EEE        KKK       İ     RRRRRR");
+        System.out.println("E         B     B   U     U   B     B   E          K   K     İ     R     R");
+        System.out.println("EEEEEEE   BBBBBB     UUUUU    BBBBBB    EEEEEEE   K     K    İ     R     R");
+    }
+	
+
+}
